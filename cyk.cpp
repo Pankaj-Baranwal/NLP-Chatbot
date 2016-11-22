0cdd7c05b6aaec1cb125411d1e819aa441cfae09
Pankaj-Baranwal <pankajisadeveloper@gmail.com>
2016-11-22 20:31:35 +0530

    commenting of global variables and removing redundant code

--- cyk.cpp
+++ cyk.cpp
@@ -5,30 +5,23 @@
 #include<cassert>
 #include<iomanip>
 using namespace std;
- 
-string dictionary[] = {
-    "fish",
-    "is",
-    "in",
-    "the",
-    "pond"
-};
 
 string value[] = {
-    "A fish pond boy girl ocean food table room knowledge ball",
+    // A => Nouns
+    "A fish pond boy girl ocean food table room knowledge ball apple",
+    // B => Verbs
     "B eats swims ate saw put",
+    // C => Prepositions
     "C of about on in with",
-    "D a the this that",
-}
+    // D => Determinants
+    "D a an the this that",
+};
 
 string rule[] = {
     "S NP VP",
-    "VP VP PP",
-    "VP V NP",
+    "VP VP PP|V NP|V",
     "PP P NP",
-    "NP Det N",
-    "NP NP PP",
-    "NP N",
+    "NP Det N|NP PP|N",
     "N A",
     "V B",
     "P C",
@@ -36,17 +29,19 @@ string rule[] = {
 };
 
 #define MAX 100
-#define for(i,a,b) for(i=a;i<b; i++)
+#define fr(i,a,b) for(i=a;i<b; i++)
  
-string gram[MAX][MAX];  //to store entered grammar
-string dpr[MAX];
-int p,np;       //np-> number of productions
+string gram[MAX][MAX];  // Each row stores dpr for that production.
+string dpr[MAX];        // dpr-> Each index stores one rule for a production till p.
+int p;                  // p-> Max number of combination of rules for each production. 
+                        // Example: For S, p = 2
+int np = 11;            // np-> number of productions
  
 inline string concat( string a, string b)   //concatenates unique non-terminals
 {
     int i;
     string r=a;
-    for(i,0,b.length())
+    fr(i,0,b.length())
         if(r.find(b[i]) > r.length())
             r+=b[i];
     return (r);
@@ -58,7 +53,7 @@ inline void break_gram(string a)    //seperates right hand side of entered gramm
     p=0;
     while(a.length())
     {
-        i=a.find("|");
+        i = a.find("|");
         if(i>a.length())
         {
             dpr[p++] = a;
@@ -72,27 +67,11 @@ inline void break_gram(string a)    //seperates right hand side of entered gramm
     }
 }
  
-inline int lchomsky(string a)   //checks if LHS of entered grammar is in CNF
-{
-    if(a.length()==1 && a[0]>='A' && a[0]<='Z')
-        return 1;
-    return 0;
-}
- 
-inline int rchomsky(string a)   //checks if RHS of grammar is in CNF
-{
-    if (a.length() == 1 && a[0]>='a' && a[0] <='z')
-        return 1;
-    if (a.length()==2 && a[0]>='A' && a[0]<='Z' && a[1]>='A' && a[1]<='Z' )
-        return 1;
-    return 0;
-}
- 
 inline string search_prod(string p) //returns a concatenated string of variables which can produce string p
 {
     int j,k;
     string r="";
-    for(j,0,np)
+    fr(j,0,np)
     {
         k=1;
         while(gram[j][k] != "")
@@ -111,8 +90,8 @@ inline string gen_comb(string a, string b)  //creates every combination of varia
 {
     int i,j;
     string pri=a,re="";
-    for(i,0,a.length())
-        for(j,0,b.length())
+    fr(i,0,a.length())
+        fr(j,0,b.length())
         {
             pri="";
             pri=pri+a[i]+b[j];
@@ -125,41 +104,27 @@ int main()
 {
     int i,pt,j,l,k;
     string a,str,r,pr,start;
-    cout<<"\nEnter the start Variable ";
-    cin >> start;
-    cout<<"\nNumber of productions ";
-    cin >> np;
-    for(i,0,np)
+    start = "S";
+    fr(i,0,np)
     {
-        cin >> a;
-        pt=a.find("->");
+        a = rule[i];
+        pt = a.find(" ");
         gram[i][0] = a.substr(0,pt);
-        if (lchomsky(gram[i][0]) == 0)
-        {
-            cout<<"\nGrammar not in Chomsky Form";
-            abort();
-        }
-        a = a.substr(pt+2, a.length());
+        a = a.substr(pt+1, a.length());
         break_gram(a);
-        for(j,0,p)
+        fr(j,0,p)
         {
             gram[i][j+1]=dpr[j];
-            if (rchomsky(dpr[j]) == 0)
-            {
-                cout<<"\nGrammar not in Chomsky Form";
-                abort();
-            }
         }
     }
     string matrix[MAX][MAX],st;
-    cout<<"\nEnter string to be checked : ";
-    cin >> str;
-    for(i,0,str.length())       //Assigns values to principal diagonal of matrix
+    str = "fish swims in the ocean";
+    fr(i,0,str.length())       //Assigns values to principal diagonal of matrix
     {
         r="";
         st = "";
         st+=str[i];
-        for(j,0,np)
+        fr(j,0,np)
         {
             k=1;
             while(gram[j][k] != "")
@@ -174,12 +139,12 @@ int main()
         matrix[i][i]=r;
     }
     int ii,kk;
-    for(k,1,str.length())       //Assigns values to upper half of the matrix
+    fr(k,1,str.length())       //Assigns values to upper half of the matrix
     {
-        for(j,k,str.length())
+        fr(j,k,str.length())
         {
             r="";
-            for(l,j-k,j)
+            fr(l,j-k,j)
             {
                 pr = gen_comb(matrix[j-k][l],matrix[l+1][j]);
                 r = concat(r,pr);
@@ -188,11 +153,11 @@ int main()
         }
     }
  
-    for(i,0,str.length())   //Prints the matrix
+    fr(i,0,str.length())   //Prints the matrix
     {
         k=0;
         l=str.length()-i-1;
-        for(j,l,str.length())
+        fr(j,l,str.length())
         {
             cout<<setw(5)<<matrix[k++][j]<<" ";
         }
@@ -200,7 +165,7 @@ int main()
     }
              
     int f=0;
-    for(i,0,start.length())
+    fr(i,0,start.length())
         if(matrix[0][str.length()-1].find(start[i]) <= matrix[0][str.length()-1].length())   //Checks if last element of first row contains a Start variable
         {
             cout<<"String can be generated\n";
